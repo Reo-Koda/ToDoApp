84e1b606203b65fdcb07ce1ac24b76faa41838f3
Reo-Koda <matoryoshika719@gmail.com>
2025-04-12 23:24:51 +0900

    modify mysql

--- mysql/db/init.sql
+++ mysql/db/init.sql
@@ -3,13 +3,14 @@ use todo_db;
 
 CREATE TABLE IF NOT EXISTS Users (
   user_id INT AUTO_INCREMENT PRIMARY KEY,
-  user_name VARCHAR(30) NOT NULL
+  user_name VARCHAR(30) NOT NULL,
+  password VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Posts(
   post_id INT AUTO_INCREMENT PRIMARY KEY,
   user_id INT NOT NULL,
-  title TEXT,
+  title TEXT NOT NULL,
   about TEXT,
   post_date DATETIME NOT NULL,
   FOREIGN KEY (user_id) REFERENCES Users(user_id)
